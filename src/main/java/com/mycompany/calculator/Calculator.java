6b76d975f778ec6a40a6129b5be9c58ad0d13922
breezyaloo123 <alassanesall771@gmail.com>
2020-11-29 21:13:35 +0000

    We make some comments for all the methods

--- src/main/java/com/mycompany/calculator/Calculator.java
+++ src/main/java/com/mycompany/calculator/Calculator.java
@@ -14,28 +14,32 @@ package com.mycompany.calculator;
  */
 public class Calculator {
 
+   //Cette methode nous permet de faire la somme de deux entiers
 	public int sum(int a, int b) {
 		return a + b;
 	}
-
+	//Cette methode nous permet de faire la soustraction de deux entiers
 	public int minus(int a, int b) {
 		return a - b;
 	}
-
+	//Cette methode nous permet de faire la division de deux entiers
 	public int divide(int a, int b) {
 		return a / b;
 	}
-
+	//Cette methode nous permet de faire la multiplication de deux entiers
 	public int multiply(int a, int b) {
 		return a * b;
 	}
+	//Cette methode nous permet de savoir le minimum entre deux entiers
 	public int min(int a,int b) {
 			return Math.min(a,b);
 	}
+	//Cette methode nous permet de savoir le maximum entre deux entiers
 	public int max (int a , int b) {
 		 
 			return Math.max(a,b);
 	}
+	//Cette methode nous permet de savoir le plus petit entier dans un tableau donne
         public int minElement(int[] liste){
             int min=liste[0];
             for(int i:liste){
@@ -45,6 +49,7 @@ public class Calculator {
             }
             return min;
         }
+	//Cette methode nous permet de savoir le plus grand entier dans un tableau donne
         public int maxElement(int[] list){
             int max=list[0];
             for(int i : list){
